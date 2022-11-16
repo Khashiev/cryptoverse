c6fd9093c39707d15caaa150618bfb2dfbdfd678
Nazir Khashiev <nxashiev@gmail.com>
2022-11-16 10:04:05 +0300

    added dependencies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+// import {Route, Routes, Link} from 'react-router-dom';
+import { Layout, Typography, Space } from "antd";
 
 const App = () => {
   return (
-    <div>
-      <h1>Cryptoverse</h1>
+    <div className="app">
+      <div className="header"></div>
+      <div className="main"></div>
+      <div className="footer"></div>
     </div>
   );
 };
