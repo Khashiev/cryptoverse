b1be0dec831dc1cdd5cd9df56168e8d6205554c3
Nazir Khashiev <nxashiev@gmail.com>
2022-11-16 11:48:49 +0300

    added basic styles and components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
 import React from "react";
-// import {Route, Routes, Link} from 'react-router-dom';
+import "./App.css";
+import { Route, Routes, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
+
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from "./components";
 
 const App = () => {
   return (
     <div className="app">
-      <div className="header"></div>
-      <div className="main"></div>
-      <div className="footer"></div>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+
+        <div className="footer">
+          <Typography.Text leve={2} style={{ color: "white", textAlign: "center" }}>
+            Cryptoverse <br />
+            All rights reserved
+          </Typography.Text>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
+      </div>
     </div>
   );
 };
